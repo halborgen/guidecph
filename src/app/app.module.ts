74244461c9e08cd72741b1e702600c42a8c908d2
halborg <peterhalborg55@gmail.com>
2018-12-04 13:20:24 +0100

    1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { MapComponent } from './map/map.component';
+import { PlaceComponent } from './place/place.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    MapComponent
+    PlaceComponent
   ],
   imports: [
     BrowserModule,
